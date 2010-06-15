a72000cff1122eeaf235387571364f48bc4022a3
WardCunningham <ward@AboutUs.org>
2010-06-15 07:39:40 -0700

    simplify experiment

--- projects/blink/experiment.pl
+++ projects/blink/experiment.pl
@@ -1,19 +1,30 @@
+# conduct perception experiment
+# (apple osx specific implementation)
+
+my $stty = `stty -g`;
+my $teensy = '/dev/cu.usbmodem12341';
+mkdir 'results' unless -e 'results';
+
+die("can't find teensy\n") unless -e $teensy;
+`echo 6d0o >$teensy`;
+
 while () {
-	`open instructions.rtf`;
 	print "enter your initials: ";
-	&experiment($1) if <> =~ /(\w{2,3})/;
+	experiment($1) if <> =~ /(\w{2,3})/;
 	sleep 3;
 	`say next`;
 }
 
 sub experiment {
 	my ($initials) = @_;
+	`stty -icanon -echo`;
 	`say ready`;
 	`echo trial \`date +%s\` >> results/$initials`;
-	for $period (split(/\s+/, `jot -r 10 50 150`)) {
+	for $period (split(/\s+/, `jot -r 10 50 250`)) {
 		my $result;
 		while() {
-			$result = &trial($period);
+			`echo 5{1o${period}m0o${period}m} >$teensy`;
+			$result = getc;
 			last if $result =~ /[1-9]/;
 			`say again`;
 		}
@@ -21,10 +32,5 @@ sub experiment {
 		`echo "$period\t$result" >> results/$initials`;
 	}
 	`say thank you`;
-}
-
-sub trial {
-	my ($period) = @_;
-	`echo 5{1o${period}m0o${period}m} > /dev/cu.usbmodem12341`;
-	return `sh keystroke.sh`;
+	`stty $stty`;
 }
