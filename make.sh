1e5b7197a47c54afa5d176cbb33a9bf8150b70aa
WardCunningham <ward@AboutUs.org>
2010-07-25 10:13:08 -0700

    build for all Teensy versions

---
+++ make.sh
@@ -0,0 +1,14 @@
+for mcu in `grep 'MCU =' Makefile | cut -f 3 -d ' '`
+do echo ============= $mcu ==============
+	cat Makefile | 
+	perl -pe '
+		s/^MCU = /#MCU = /;
+		s/^#(MCU = '$mcu')/$1/;
+		' > /tmp/txtzyme_makefile
+	# head -60 /tmp/txtzyme_makefile
+	make clean
+	make -f /tmp/txtzyme_makefile
+	mv txtzyme.hex hex/$mcu.hex
+done
+make clean
+rm /tmp/txtzyme_makefile
