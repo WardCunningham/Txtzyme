4e5c82642fe657b11eb5e4b68cf564e789b08ab6
WardCunningham <ward@AboutUs.org>
2010-08-26 07:47:29 -0700

    discover devices via rom search

--- projects/onewire/test.pl
+++ projects/onewire/test.pl
@@ -30,12 +30,15 @@ sub wr { putz $_[0] ? "6f0oi60u" : "6f0o60ui" }
 sub w8 { my ($b) = @_; for (0..7) { wr($b&1); $b /= 2; } }
 sub rd { getz "6f0oiip45u" }
 sub r8 { my $b = 0; for (0..7) { $b |= (rd()<<$_) } return $b }
+sub r { my $n = 0; for my $i (0..($_[0]-1)) {$b += (r8() << 8*$i)} $b }
 
 # DS18B20 Thermometer Functions
 
 sub skip { w8 0xCC }
 sub cnvt { w8 0x44 }
 sub data { w8 0xBE }
+sub rrom { w8 0x33 }
+sub srom { w8 0xF0 }
 
 # DS18B20 Thermometer Transactions (single device)
 
@@ -46,15 +49,50 @@ sub one_data { rst; skip; data; my $c = r8; $c += 256 * r8 }
 sub temp_c { all_cnvt; 0.0625 * one_data }
 sub temp_f { 32 + 1.8 * temp_c }
 
+# ROM Search
+
+my @st = ();
+do {
+    my @at = ();
+    my @pr = ();
+    rst;
+    srom;
+    my @nx = ();
+    while ((scalar @at) < 48) {
+        my $lo = rd;
+        my $hi = rd;
+        push @pr, 2*!$hi+!$lo;
+        my $x = (scalar @st) ? shift @st : !$lo ? 0 : 1;
+        wr $x;
+        @nx = (@at,1) if $x == 0 && $hi == 0;
+        push @at, $x
+    }
+    @st = @nx;
+    print "pr: ", @pr, "\n";
+    print "at: ", @at, "\n";
+    print "st: ", @st, "\n";
+    print "\n";
+} while(@st);
+
+# Read ROM for single device
+
+# rst;
+# rrom;
+# printf "family: %x\n", r8;
+# printf "serial: %x\n", r 6;
+# printf "check: %x\n", r8;
+
+# Search ROM
+
 # Show rising (red) and falling (green) temps
 
-my ($old, $new) = (0, 0);
-while (1) {
-    ($old, $new) = ($new, temp_c());
-    printf "%3.5f c\n", $new;
-    off;
-    red if $new > $old;
-    grn if $old > $new;
-}
+# my ($old, $new) = (0, 0);
+# while (1) {
+#     ($old, $new) = ($new, temp_c());
+#     printf "%3.5f c\n", $new;
+#     off;
+#     red if $new > $old;
+#     grn if $old > $new;
+# }
 
 
