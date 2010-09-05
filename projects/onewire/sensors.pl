a446b9b72c3f8778e6491991efc7d217a2733161
WardCunningham <ward@AboutUs.org>
2010-09-04 17:29:14 -0700

    define  for onewire; json output for SensorServer via remote

--- projects/onewire/sensors.pl
+++ projects/onewire/sensors.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-# while sleep .5; do perl test.pl; done
 
 use strict;
 
@@ -24,7 +23,8 @@ off;
 
 # One-Wire Protocol (on pin F6, pwr F7, gnd F5)
 
-my $pin = "7d";
+my $pin = "5d";
+
 putz "${pin}1o5f0o";
 sub rst { getz "${pin}0o480ui60uip420u" }
 sub wr { putz $_[0] ? "${pin}0oi60u" : "${pin}0o60ui" }
@@ -50,50 +50,15 @@ sub one_data { rst; skip; data; my $c = r8; $c += 256 * r8 }
 sub temp_c { all_cnvt; 0.0625 * one_data }
 sub temp_f { 32 + 1.8 * temp_c }
 
-# ROM Search
-
-my @st = ();
-do {
-    my @at = ();
-    my @pr = ();
-    print "reset: ", rst, "\n";
-    srom;
-    my @nx = ();
-    while ((scalar @at) < 48) {
-        my $lo = rd;
-        my $hi = rd;
-        push @pr, 2*!$hi+!$lo;
-        my $x = (scalar @st) ? shift @st : !$lo ? 0 : 1;
-        wr $x;
-        @nx = (@at,1) if $x == 0 && $hi == 0;
-        push @at, $x
-    }
-    @st = @nx;
-    print "pr: ", @pr, "\n";
-    print "at: ", @at, "\n";
-    print "st: ", @st, "\n";
-    print "\n";
-} while(@st);
-
 # Read ROM for single device
 
-# rst;
-# rrom;
-# printf "family: %x\n", r8;
-# printf "serial: %x\n", r 6;
-# printf "check: %x\n", r8;
+rst;
+rrom;
+my ($family, $serial, $check) = (r(1), r(6), r(1));
+die ("expected thermometer") unless $family == 0x28;
 
-# Search ROM
+# Read temp for single device
 
-# Show rising (red) and falling (green) temps
-
-# my ($old, $new) = (0, 0);
-# while (1) {
-#     ($old, $new) = ($new, temp_c());
-#     printf "%3.5f c\n", $new;
-#     off;
-#     red if $new > $old;
-#     grn if $old > $new;
-# }
-
+my $temp = temp_f;
+print "{\n\t\"$serial\":\t$temp,\n}\n";
 
