2b1761c26564e0ac210fcdfe153f9ccd982415d9
WardCunningham <ward@AboutUs.org>
2010-10-02 18:59:10 -0700

    add sound to blink

--- projects/remote/blink.sh
+++ projects/remote/blink.sh
@@ -1,6 +1,7 @@
 while true; do
-    sleep 1; echo 1bo 100m o >/dev/cu.usbmodem12341
-    sleep 1; echo 1000{0b1o24uo 1bo24uo}>/dev/cu.usbmodem12341
-    sleep 1; echo 0b1o 100m o>/dev/cu.usbmodem12341
-    sleep 1; echo 1000{0b1o24uo 1bo24uo}>/dev/cu.usbmodem12341
+	for i in "1bo 100m o" "1000{0b1o24uo 1bo24uo}" "0b1o 100m o" "1000{0b1o24uo 1bo24uo}"
+		do echo 5{2b1o6b0o 170u 2b0o6b1o 170u} 2b0oi6b0oi
+		echo $i
+		sleep 1
+	done >/dev/cu.usbmodem12341
 done
