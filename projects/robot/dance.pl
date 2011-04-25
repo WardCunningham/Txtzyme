54e62817e118fc1d1851bcb7a735971ff8c31d00
WardCunningham <ward@c2.com>
2011-04-24 18:03:39 -0700

    tune dance for hardwood floor

--- projects/robot/dance.pl
+++ projects/robot/dance.pl
@@ -14,23 +14,23 @@ sub fwd {
 
 
 sub run {
-	my $p = 2000;
+	my $p = 1000;
 	my ($r, $l) = map(int($p*abs($_)), @_);
 	my ($p1, $p2, $p3)  = (min($r,$l), max($r,$l)-min($r,$l), $p-max($r,$l));
 	my ($f, $s) = $r > $l ? ('7b','0d') : ('0d','7b'); 
-	"20 {${f}1o ${s}1o ${p1}u ${s}0o ${p2}u ${f}0o ${p3}u}";
+	"10 {${f}1o ${s}1o ${p1}u ${s}0o ${p2}u ${f}0o ${p3}u}";
 }
 
 sub go {
 	my ($r, $l) = @_;
-	#print '7f1o ';
+	#print '4b1o ';
 	print fwd '2b', '3b', $r;
 	print fwd '3d', '2d', $l;
-	#print '7f0o ';
+	#print '4b0o ';
 	print run $r, $l;
 	print "\n";
 }
 
-for my $t (1..200) {
-	go sin($t/20), cos($t/20);
+for my $t (1..1500) {
+	go sin($t/30), cos($t/30);
 }
