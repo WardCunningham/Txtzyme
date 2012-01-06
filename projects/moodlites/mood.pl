f3dbf8c97819e03cc98c972121bd450a47bfe569
Ward Cunningham <ward@c2.com>
2012-01-06 10:27:49 -0800

    lock txtzyme device; nextcolor changes all bulbs

--- projects/moodlites/mood.pl
+++ projects/moodlites/mood.pl
@@ -1,3 +1,4 @@
+exit;
 
 # Textzyme
 
@@ -9,7 +10,7 @@ open (T, ">/dev/cu.usbmodem123451") or die($!);
 sub start { print T "1o" }
 sub zero  { print T "0o1o2u" }
 sub one   { print T "0o2u1o" }
-sub stop  { print T "0o\n" }
+sub stop  { print T "0o100m\n" }
 
 sub bits  { my ($n,$v) = @_; for (my $i=$n-1; $i>=0; $i--) { $v>>$i&1 ? one() : zero() }}
 sub bulb  { bits(6, $_[0]) }
@@ -21,6 +22,12 @@ sub set {
   start(); bulb($bulb); light($light); color($blue); color($green); color($red); stop();
 }
 
+sub all {
+  my ($red, $green, $blue) = @_;
+  for (1..50) { set($_, 255, $red, $green, $blue) }
+}
+
+
 # SensorServer json (furnace plenum code c19356)
 
 `/usr/bin/curl -s guest:please\@jd.local:4567/one/5d` =~ /"c19356":\s*(\d+),/;
@@ -30,10 +37,15 @@ my $heat = sprintf("%.1f", ($1/16.0)*9/5+32);
 
 my $bulb = int(rand(50))+1;
 
-if (-f 'next.txt') { set($bulb, 255, $c[0], $c[1], $c[2]) if @c = split(/\./, `cat next.txt; rm next.txt`); $show = "next" }
+use Fcntl ":flock"; 
+flock T, LOCK_EX;
+ 
+if (-f 'next.txt') { all($c[0], $c[1], $c[2]) if @c = split(/\./, `cat next.txt; rm next.txt`); $show = "next" }
 elsif ($heat > 90) { set($bulb, 255, 15, 0, 0); $show = "red"; }
 elsif ($heat > 70) { set($bulb, 255, 0, 15, 0); $show = "green"; }
 else               { set($bulb, 255, 0, 0, 15); $show = "blue"; }
+
+flock T, LOCK_UN; 
 
 my $now = localtime;
 print "$now\t$heat\t$bulb\t$show\n"
