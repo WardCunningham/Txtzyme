066d149baedc18b76299250c4a3490ddc0aab3d4
Ward Cunningham <ward@c2.com>
2012-01-06 10:34:36 -0800

    colorful animations

---
+++ projects/moodlites/fade.pl
@@ -0,0 +1,60 @@
+
+# Textzyme
+
+open (T, ">/dev/cu.usbmodem123451") or die($!);
+
+# GE G35 control signals
+# from http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/
+
+sub start { print T "1o" }
+sub zero  { print T "0o1o2u" }
+sub one   { print T "0o2u1o" }
+sub stop  { print T "0o\n" }
+
+sub bits  { my ($n,$v) = @_; for (my $i=$n-1; $i>=0; $i--) { $v>>$i&1 ? one() : zero() }}
+sub bulb  { bits(6, $_[0]) }
+sub light { bits(8 ,$_[0]) }
+sub color { bits(4, $_[0]) }
+
+sub set {
+  my ($bulb, $light, $red, $green, $blue) = @_;
+  start(); bulb($bulb); light($light); color($blue); color($green); color($red); stop();
+}
+
+sub all {
+  my ($red, $green, $blue) = @_;
+  for (1..50) { set($_, 255, $red, $green, $blue) }
+}
+
+my @snr = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+my @sn = ();
+push(@sn, @snr);
+push(@sn, reverse(@snr));
+push(@sn, map(0,@snr));
+$sn = @sn;
+$pn = $sn/3;
+
+sub sn {
+  my $n = $_[0]%$sn;
+  $sn[$n];
+}
+
+sub fade {
+  my ($r,$g,$b) = @_;
+  for $n (1..$sn) {
+    for (1..50) {
+      set ($_, 255, sn($_+$n+$pn*$r), sn($_+$n+$pn*$g),sn($_+$n+$pn*$b));
+    }
+  }
+}
+
+
+use Fcntl ":flock"; 
+flock T, LOCK_EX;
+ 
+while(1) {
+  fade(0,1,2);
+}
+
+flock T, LOCK_UN; 
+
---
+++ projects/moodlites/wipe.pl
@@ -0,0 +1,46 @@
+
+# Textzyme
+
+open (T, ">/dev/cu.usbmodem123451") or die($!);
+
+# GE G35 control signals
+# from http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/
+
+sub start { print T "1o" }
+sub zero  { print T "0o1o2u" }
+sub one   { print T "0o2u1o" }
+sub stop  { print T "0o20m\n" }
+
+sub bits  { my ($n,$v) = @_; for (my $i=$n-1; $i>=0; $i--) { $v>>$i&1 ? one() : zero() }}
+sub bulb  { bits(6, $_[0]) }
+sub light { bits(8 ,$_[0]) }
+sub color { bits(4, $_[0]) }
+
+sub set {
+  my ($bulb, $light, $red, $green, $blue) = @_;
+  start(); bulb($bulb); light($light); color($blue); color($green); color($red); stop();
+}
+
+sub all {
+  my ($red, $green, $blue) = @_;
+  for (1..50) { set($_, 255, $red, $green, $blue) }
+}
+
+sub rn {
+  $rn = @rn = (0, 1, 3, 7, 15);
+  $rn[rand($rn)];
+}
+
+sub every {
+  for (1..50) {set($_, 255, rn(),rn(),rn()) }
+  for (1..50) {set(52-$_, 255, rn(),rn(),rn()) }
+}
+
+
+use Fcntl ":flock"; 
+flock T, LOCK_EX;
+ 
+every();
+
+flock T, LOCK_UN; 
+
