5996ba06172ad9b06203071d6b40503c28e290f2
Ward Cunningham <ward@c2.com>
2012-01-29 12:31:24 -0800

    off for the season

---
+++ projects/moodlites/off.pl
@@ -0,0 +1,36 @@
+
+# Textzyme
+
+open (T, ">/dev/cu.usbmodem123451") or die($!);
+
+# GE G35 control signals
+# from http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/
+
+sub start { print T "1o" }
+sub zero  { print T "0o1o2u" }
+sub one   { print T "0o2u1o" }
+sub stop  { print T "0o\n" }
+
+sub bits  { my ($n,$v) = @_; for (my $i=$n-1; $i>=0; $i--) { $v>>$i&1 ? one() : zero() }}
+sub bulb  { bits(6, $_[0]) }
+sub light { bits(8 ,$_[0]) }
+sub color { bits(4, $_[0]) }
+
+sub set {
+  my ($bulb, $light, $red, $green, $blue) = @_;
+  start(); bulb($bulb); light($light); color($blue); color($green); color($red); stop();
+}
+
+sub all {
+  my ($red, $green, $blue) = @_;
+  for (1..50) { set($_, 255, $red, $green, $blue) }
+}
+
+
+use Fcntl ":flock"; 
+flock T, LOCK_EX;
+ 
+all(0,0,0);
+
+flock T, LOCK_UN; 
+
