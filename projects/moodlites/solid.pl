c2a92b6594f066908c85f5d9a470cd443cdc45a5
Ward Cunningham <ward@c2.com>
2012-12-09 05:05:58 -0800

    solid color shifting in from file

---
+++ projects/moodlites/solid.pl
@@ -0,0 +1,63 @@
+# Textzyme
+
+open (T, ">".`ls /dev/cu.usbmodem*`) or die($!);
+
+# GE G35 control signals
+# from http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/
+
+sub start { print T "1o" }
+sub zero  { print T "0o1o2u" }
+sub one   { print T "0o2u1o" }
+sub stop  { print T "0o\n" }
+
+sub bits  { my ($n,$v) = @_; for (my $i=$n-1; $i>=0; $i--) { $v>>$i&1 ? one() : zero() }}
+sub bulb  { bits(6, $_[0]) }
+sub light { bits(8 ,$_[0]) }
+sub color { bits(4, $_[0]) }
+
+sub set {
+  my ($bulb, $light, $red, $green, $blue) = @_;
+  start(); bulb($bulb); light($light); color($blue); color($green); color($red); stop();
+}
+
+sub all {
+  my ($red, $green, $blue) = @_;
+  for (1..50) { set($_, 255, $red, $green, $blue) }
+}
+
+sub rn {
+  int($_[0]*rand());
+}
+
+sub br {
+  return 0 if $_[0]<0.3;
+  int(15.9*$_[0]);
+}
+
+sub sc {
+  return int($_[0]/16);
+}
+
+@bulbs = [];
+for (1..50) {
+  push @bulbs, "0,0,0";
+}
+
+sub writeBulbs {
+  open B, '>/Users/ward/g35/newBulbs';
+  print B @bulbs;
+  close B;
+  rename '/Users/ward/g35/newBulbs', '/users/ward/g35/bulbs';
+}
+
+while (1) {
+  $_ =`cat /Users/ward/g35/color`;
+  push @bulbs, $_;
+  shift @bulbs;
+  for $bulb (1..50) {
+    ($r, $g, $b) = $bulbs[51-$bulb] =~ /(\d+)/g;
+    set $bulb, 255, sc($r), sc($g), sc($b);
+  }
+  writeBulbs();
+  select undef, undef, undef, 0.1;
+}
